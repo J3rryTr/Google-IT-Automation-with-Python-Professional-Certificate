fd00b15c7756bb7dd0b7773fdfe6c60e935847ef
unknown <trungtran20802@gmail.com>
2024-02-11 15:41:15 +0700

    push

---
+++ C2_Using_Python_to_Interact_with_the_Operating_System/module2.py
@@ -0,0 +1,8 @@
+import re
+
+# Regex examples
+# r”\d{3}-\d{3}-\d{4}”  This line of code matches U.S. phone numbers in the format 111-222-3333.
+# r”^-?\d*(\.\d+)?$”  This line of code matches any positive or negative number, with or without decimal places.
+# r”^[a-zA-Z0-9_]*$”  This line of code matches any alphanumeric string that contains no spaces.
+# r”/^(.+)/([^/]+)$/” This line of code matches any path and filename.
+
